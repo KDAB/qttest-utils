45a2eace6e2dc350f3dcb98aca4c5ffad8fb8994
Sergio Martins <sergio.martins@kdab.com>
2024-06-04 22:45:19 +0100

    ci: Also test Qt 6

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 cmake --preset=dev -S test/qt_test && \
 cmake --build test/qt_test/build-dev && \
+tsc && node out/test.js && \
+rm -rf test/qt_test/build-dev && \
+cmake --preset=dev6 -S test/qt_test && \
+cmake --build test/qt_test/build-dev && \
 tsc && node out/test.js
